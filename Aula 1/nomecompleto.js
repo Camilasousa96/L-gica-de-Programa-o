14a6af71df5b3b0992c10cd9e731067238ec46e7
Camilasousa96 <csilvatst@gmail.com>
2024-09-04 21:33:07 -0300

    Add files via upload

---
+++ Aula 1/nomecompleto.js
@@ -0,0 +1,10 @@
+/* Declare duas variáveis chamada "nome" e "sobrenome",
+atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira
+variável chamada "nomeCompleto" e imprima ao usuário*/
+
+let nome = "Camila";
+
+let sobrenome = "Sousa";
+
+let nomeCompleto = nome + " " + sobrenome;
+console.log("Seu nome completo é:" ,nomeCompleto);
