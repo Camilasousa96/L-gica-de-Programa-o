704a361bfe2cab9df7ae81ef71893332e19e2e5a
Camilasousa96 <csilvatst@gmail.com>
2024-09-10 19:33:31 -0300

    Add files via upload

---
+++ Aula 4/ex8.js
@@ -0,0 +1,3 @@
+var numero = parseInt(prompt("Informe um numero"))
+var resultado = numero%2 == 0 ? "Par" : "Ímpar"
+console.log(resultado) /*Reduzindo(Ternários) o exercício 5
---
+++ Aula 4/ex9.js
@@ -0,0 +1,3 @@
+var numero = parseInt(prompt("Informe um número"))
+var resultado = numero > 0 ? "Positivo!" : numero < 0 ? "Negativo" : "Zero"
+console.log(resultado) /*Reduzindo(Ternários) o exercício 6
