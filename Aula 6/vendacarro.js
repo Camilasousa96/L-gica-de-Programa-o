ee8acc7c53b65e2c049037dae6a6027a8a2aea39
Camilasousa96 <csilvatst@gmail.com>
2024-09-12 19:21:47 -0300

    Add files via upload

---
+++ Aula 6/vendacarro.js
@@ -0,0 +1,33 @@
+carro = prompt("Digite o automóvel que você deseja comprar: ");
+
+switch(carro) {
+    case "hatch":
+        console.log("Compra efetuada com sucesso");
+        break
+    case "sedan":
+        console.log("Tem certeza que não prefere este modelo?")
+        break
+    case "motocicleta":
+        console.log("Tem certeza que não prefere este modelo?")
+        break
+    case "caminhonete":
+      console.log("Tem certeza que não prefere este modelo?")
+    default:
+        console.log("Não trabalhamos com este tipo de automóvel aqui")
+}
+
+/*automovel = prompt("Digite o tipo de automóvel que você deseja compra
+    
+    switch(automovel") {
+        case "hatch":
+            console.log("Compra efetuada com sucesso");
+            break
+
+        case "sedan":
+        case "motocicleta":
+        case "caminhonete":
+            console.log("Tem certeza que não prefere este modelo?")
+            break
+        default:
+            console.log("Não trabalhamos com este tipo de automóvel aqui!")
+}*/
