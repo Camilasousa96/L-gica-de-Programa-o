6c23f943b6ea6b3e7537220c2dcedf15d609752d
Camilasousa96 <csilvatst@gmail.com>
2024-09-23 21:32:22 -0300

    Add files via upload

--- Aula 12/ex2array.js
+++ Aula 12/ex2array.js
@@ -4,6 +4,7 @@ exibindo o resultado.*/
 let numeros = [1, 2, 3, 4, 5 ,6, 7, 8, 9, 10]; // Crio array de números
 let soma = 0;// Inicializo a variável para armazenar a soma
 
+//uso loop for para somar os valores
 for (i=0; i<numeros.length; i++){
     soma += numeros[i];
 }
